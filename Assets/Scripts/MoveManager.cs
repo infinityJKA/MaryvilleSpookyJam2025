a1d6715c7a82681dc49322cef926517642d83574
Justout <jkajustin@gmail.com>
2025-10-23 10:51:52 -0500

    player turns when moving

--- Assets/Scripts/MoveManager.cs
+++ Assets/Scripts/MoveManager.cs
@@ -25,8 +25,14 @@ public class MoveManager : MonoBehaviour
     public void Move(Vector2 movement)
     {
         Vector3 playerMove = new Vector3(movement.x, 0f, movement.y);
-        playerMove.Normalize();
-        playerMove = playerMove * moveSpeed;
+        
+        Quaternion targetRotation = Quaternion.LookRotation(playerMove);
+        player.transform.rotation = targetRotation;
+        Debug.Log("targetRotation: " + targetRotation);
+
+        //playerMove.Normalize();
+        //playerMove = playerMove * moveSpeed;
+        playerMove = Vector3.forward * moveSpeed;
         player.transform.Translate(playerMove);
     }
 }
