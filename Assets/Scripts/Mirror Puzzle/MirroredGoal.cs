6767dfa0a89f45ff4a2ded2c488bea2992df753a
Justout <jkajustin@gmail.com>
2025-10-24 15:45:09 -0500

    mirror puzzle

---
+++ Assets/Scripts/Mirror Puzzle/MirroredGoal.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MirroredGoal : MonoBehaviour
+{
+    public List<DialogueLine> dialogue;
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Collided with layer: " + collision.gameObject.tag);
+        if (collision.gameObject.CompareTag("Mirrored"))
+        {
+            GameManager.instance.dialogueManager.StartDialogue(dialogue);
+        }
+    }
+}
